e6375f85b7539f13bcf9fccbfdc6f004a6eae2d1
Aaron Shannon <ayren.shannon@me.com>
2025-08-20 00:18:28 -0700

    rick face script

--- ui/hud/hud.gd
+++ ui/hud/hud.gd
@@ -3,6 +3,13 @@ extends CanvasLayer
 
 @onready var health_label = $main_box/MarginContainer/MarginContainer/display_seperator/health/header_content_seperator/health_content
 
+# rick head icon
+@onready var chill_icon = %rick_head
+
+# preloaded textures
+@onready var full_chill_icon = preload("res://ui/sprite/rick_glasses.png")
+@onready var low_chill_icon = preload("res://ui/sprite/rick_no_glasses.png")
+
 func _ready() -> void:
 	g.player.health_changed.connect(_on_health_changed)
 	
@@ -12,6 +19,11 @@ func _process(delta: float) -> void:
 	
 func _on_health_changed(new_health: int) -> void:
 	health_label.text = str(new_health)
+
+	if new_health <= 75:
+		chill_icon.texture = low_chill_icon
 	
+	else:
+		chill_icon.texture = full_chill_icon
 	
 	
