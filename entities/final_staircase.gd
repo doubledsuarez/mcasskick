419b12a8fc3f0370551b1730774825ffb7f9e60b
ohnoitsmonroe <milton.p.monroe@gmail.com>
2025-08-24 11:00:53 -0700

    Staircase: Fixed final staircase

--- entities/final_staircase.gd
+++ entities/final_staircase.gd
@@ -3,15 +3,27 @@ extends Node3D
 # Final staircase
 # this will rise all the staircase segments based on how many figurines the player has
 
-var segments := []
+var segments := {}
 
 var player_near := false
 
 func _ready():
-	for child in get_children():
-		if child is StaircaseSegment:
-			segments.append(child)
+	#for child in get_children():
+		#if child is StaircaseSegment:
+			#segments.append(child)
+	segments = {
+		1:$Staircase,
+		2:$Staircase2,
+		3:$Staircase3,
+		4:$Staircase4,
+		5:$Staircase5,
+		6:$Staircase6,
+		7:$Staircase7,
+		8:$Staircase8
+	}
+			
 	await get_tree().create_timer(.1).timeout
+	
 	g.player.item_picked_up.connect(update_stairs)
 
 
@@ -20,12 +32,13 @@ func _on_area_3d_body_entered(body):
 	update_stairs("")
 
 func update_stairs(_item_name=""):
-	print("Rinal staircase Player near " + str(g.player.figurine_count))
 	if player_near:
 		for segment in g.player.figurine_count:
-			print("Final staircase rising stair")
-			segments[segment].rise()
-			await get_tree().create_timer(2.0).timeout
+			segment += 1
+
+			if segments.has(segment):
+				segments[segment].rise()
+				await get_tree().create_timer(1.0).timeout
 
 
 func _on_area_3d_body_exited(body):
