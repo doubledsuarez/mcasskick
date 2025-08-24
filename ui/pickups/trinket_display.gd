709548c9a9013a1ddcbb2899b94944c947133629
ohnoitsmonroe <milton.p.monroe@gmail.com>
2025-08-23 20:23:37 -0700

    Figurine Previews: Added textures and display node

---
+++ ui/pickups/trinket_display.gd
@@ -0,0 +1,12 @@
+extends MarginContainer
+
+@export var figurine_name := ""
+@export var figurine_texture : Texture2D
+@onready var trinket_display := $MarginContainer/trinket_display
+
+func _ready():
+	trinket_display.visible = false
+	trinket_display.texture = figurine_texture
+
+func set_collected():
+	trinket_display.visible = true
