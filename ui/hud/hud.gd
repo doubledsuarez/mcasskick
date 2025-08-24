0f16f03b4309c88ab05a0e80240c2bd87e3b9d89
ohnoitsmonroe <milton.p.monroe@gmail.com>
2025-08-23 20:23:53 -0700

    Hud: Hooked up the figurine display to work with the player texture

--- ui/hud/hud.gd
+++ ui/hud/hud.gd
@@ -2,9 +2,11 @@ extends CanvasLayer
 
 
 @onready var health_label = $main_box/MarginContainer/MarginContainer/display_seperator/health/header_content_seperator/health_content
+@export var trinket_grid : GridContainer = null
 
 func _ready() -> void:
 	g.player.health_changed.connect(_on_health_changed)
+	g.player.item_picked_up.connect(_item_picked_up)
 	
 	
 func _process(delta: float) -> void:
@@ -13,5 +15,13 @@ func _process(delta: float) -> void:
 func _on_health_changed(new_health: int) -> void:
 	#health_label.text = str(new_health)
 	pass
-	
+
+func _item_picked_up(figurine_name:String):
+	if trinket_grid != null:
+		# Loop through the TriketGrids children, and see if any have a figurine name
+		# that matches the item picked up name
+		for display in trinket_grid.get_children():
+			if display.figurine_name == figurine_name:
+				if display.has_method("set_collected"):
+					display.set_collected()
 	
