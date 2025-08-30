2edb84fa0e17a97e1a01766e35e4dd69ea4435a7
suarez <danny@suarez.fm>
2025-08-30 03:13:15 -0700

    dded bird and cow dialogue

--- helpers/interactable.gd
+++ helpers/interactable.gd
@@ -1,7 +1,14 @@
 extends Area3D
 
+const BALLOON = preload("res://ui/balloon.tscn")
+
 @export var dialogue_resource: DialogueResource
 @export var dialogue_start: String = "start"
 
 func action() -> void:
-	DialogueManager.show_example_dialogue_balloon(dialogue_resource, dialogue_start)
+	g.player.immobile = true
+	g.player.shooting_enabled = false
+	g.player.in_dialogue = true
+	g.player.jumping_enabled = false
+	
+	DialogueManager.show_dialogue_balloon_scene(BALLOON, dialogue_resource, dialogue_start)
